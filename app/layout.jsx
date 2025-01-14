bd6d2e81b12877a05dc0421672ee928dcd85eb92
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-01-14 23:12:26 +0530

    updated analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 import { Toaster as ToasterSonner } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
+          <Analytics />
           <Toaster />
           <ToasterSonner />
         </body>
