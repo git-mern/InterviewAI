81fe9cc27363daaa30721962b78441accdad497f
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-01-19 23:34:46 +0530

    Added Footer

---
+++ components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center my-4">
+      <p className="text-sm text-neutral-600">
+        &#169; interviewai-nu.vercel.app, 2025
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
