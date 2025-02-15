f7519efa8dc8846c7e05deb8e230ad8ae4c114fd
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-02-16 00:52:28 +0530

    Make some changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
           <Analytics />
-          <Adsense />
+          {/* <Adsense /> */}
           <Footer />
 
           <Toaster />
